442e78b62f95bf8ce5d9c9660572b172f3020878
Marcelo H. Rocha <marcelo.h.rocha1997@gmail.com>
2021-12-04 18:06:56 -0300

    [FIX] Error 413 - Request Entity too Large

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './configs/winston.config';
+import * as bodyParser from 'body-parser';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule, { logger });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
